81aba26039c3ed6c24fcb6e3ac23a3f53b1c8239
Zalan Fasanga <zalan.fasanga@gmail.com>
2025-06-26 23:40:50 -0400

    implemented motion blur

--- src/ray.h
+++ src/ray.h
@@ -7,12 +7,17 @@ class ray {
     public:
         ray() {}
 
+        ray(const point3& origin, const point3& direction, double time) 
+           : orig(origin), dir(direction), tm(time) {}
+
         ray(const point3& origin, const point3& direction) 
-           : orig(origin), dir(direction) {}
+           : ray(origin, direction, 0) {}
 
 
         const point3& origin()    const { return orig; }
         const vec3&   direction() const { return dir; }
+
+        double time() const { return tm; }
 
         point3 at(double t) const {
             return orig + t*dir;
@@ -21,6 +26,7 @@ class ray {
     private:
         point3 orig;
         vec3 dir;
+        double tm;
 };
 
 #endif
--- src/sphere.h
+++ src/sphere.h
@@ -5,11 +5,18 @@
 
 class sphere : public hittable {
     public: 
-        sphere(const point3& center, double radius, shared_ptr<material> mat)
-        : center(center), radius(std::fmax(0,radius)), mat(mat) {}
+        // Stationary sphere
+        sphere(const point3& static_center, double radius, shared_ptr<material> mat)
+        : center(static_center, vec3(0, 0, 0)), radius(std::fmax(0,radius)), mat(mat) {}
+
+        // Moving sphere
+        sphere(const point3& center1, const point3& center2, double radius, shared_ptr<material> mat)
+        : center(center1, center2 - center1), radius(std::fmax(0,radius)), mat(mat) {}
+
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-            vec3 oc = center - r.origin();
+            point3 current_center = center.at(r.time());
+            vec3 oc = current_center - r.origin();
             auto a = r.direction().length_squared();
             auto h = dot(r.direction(), oc);
             auto c = oc.length_squared() - radius*radius;
@@ -32,7 +39,7 @@ class sphere : public hittable {
             rec.t = root;
             rec.p = r.at(rec.t);
             //for a sphere, divifing by radius will normalize
-            vec3 outward_normal = (rec.p - center) / radius;
+            vec3 outward_normal = (rec.p - current_center) / radius;
             rec.set_face_normal(r, outward_normal);
             rec.mat = mat;
 
@@ -40,7 +47,7 @@ class sphere : public hittable {
         }
 
     private:
-        point3 center;
+        ray center;
         double radius;
         shared_ptr<material> mat;
 };
