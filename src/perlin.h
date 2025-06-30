ee91ac6567d5ada38afe2f1255b66aaa3c693771
Zalan Fasanga <zalan.fasanga@gmail.com>
2025-06-29 21:52:39 -0400

    Implemented perlin noise

--- src/perlin.h
+++ src/perlin.h
@@ -7,7 +7,7 @@ class perlin {
     public:
         perlin() {
             for(int i = 0; i < point_count; i++) {
-                randfloat[i] = random_double();
+                randvec[i] = unit_vector(vec3::random(-1,1));
             }
 
             perlin_generate_perm(perm_x);
@@ -19,32 +19,28 @@ class perlin {
             auto u = p.x() - std::floor(p.x());
             auto v = p.y() - std::floor(p.y());
             auto w = p.z() - std::floor(p.z());
-            // hermitian smoothing
-            u = u*u*(3-2*u);
-            v = v*v*(3-2*v);
-            w = w*w*(3-2*w);
 
 
             auto i = int(std::floor(p.x()));
             auto j = int(std::floor(p.y()));
             auto k = int(std::floor(p.z()));
-            double c[2][2][2];
+            vec3 c[2][2][2];
 
             for (int di = 0; di < 2; di++) 
                 for (int dj = 0; dj < 2; dj++)
                     for (int dk = 0; dk < 2; dk++)
-                        c[di][dj][dk] = randfloat[
+                        c[di][dj][dk] = randvec[
                             perm_x[(i + di) & 255] ^
                             perm_y[(j + dj) & 255] ^
                             perm_z[(k + dk) & 255]
                         ];
 
-            return trilinear_interp(c, u, v, w);
+            return perlin_interp(c, u, v, w);
         }
 
     private:
         static const int point_count = 256;
-        double randfloat[point_count];
+        vec3 randvec[point_count];
         int perm_x[point_count];
         int perm_y[point_count];
         int perm_z[point_count];
@@ -65,18 +61,25 @@ class perlin {
             }
         }
 
-        static double trilinear_interp(double c[2][2][2], double u, double v, double w) {
+        static double perlin_interp(vec3 c[2][2][2], double u, double v, double w) {
+            //Hermite cubic smoothing
+            auto uu = u*u*(3-2*u);
+            auto vv = v*v*(3-2*v);
+            auto ww = w*w*(3-2*w);
             auto accum = 0.0;
-            for (int i = 0; i < 2; i++)
-                for (int j = 0; j < 2; j++)
-                    for (int k = 0; k < 2; k++)
-                        accum += (i*u + (1-i)*(1-u))
-                               * (j*v + (1-j)*(1-v))
-                               * (k*w + (1-k)*(1-w))
-                               * c[i][j][k];
+
+            for (int i = 0; i < 2; i++) 
+                for (int j = 0; j < 2; j++) 
+                    for (int k = 0; k < 2; k++) {
+                        vec3 weight_v(u-i, v-j, w-k);
+                        accum += (i*uu + (1-i)*(1-uu))
+                               * (j*vv + (1-j)*(1-vv))
+                               * (k*ww + (1-k)*(1-ww))
+                               * dot(c[i][j][k], weight_v);
+                    }
+            
             return accum;
         }
-        
 
 };
 
--- src/texture.h
+++ src/texture.h
@@ -79,7 +79,9 @@ class noise_texture : public texture {
         noise_texture(double scale) : scale(scale) {}
 
         color value(double u, double v, const point3& p) const override {
-            return color(1,1,1) * noise.noise(scale * p);
+            // perlin interpolation returns [-1,+1] but the linear_to_gamma() function
+            // expects [0,+1] therefore we have to map the values from [-1,+1] -> [0,+1]
+            return color(1,1,1) * 0.5 * (1.0 + noise.noise(scale * p));
         }
 
     private:
