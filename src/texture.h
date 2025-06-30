fda14527c96ffc489aa9279971438fdf07c5c657
Zalan Fasanga <zalan.fasanga@gmail.com>
2025-06-29 21:39:01 -0400

    Added scale to increase noise density

--- src/texture.h
+++ src/texture.h
@@ -76,14 +76,15 @@ class image_texture : public texture {
 
 class noise_texture : public texture {
     public:
-        noise_texture() {}
+        noise_texture(double scale) : scale(scale) {}
 
         color value(double u, double v, const point3& p) const override {
-            return color(1,1,1) * noise.noise(p);
+            return color(1,1,1) * noise.noise(scale * p);
         }
 
     private:
         perlin noise;
+        double scale;
 };
 
 #endif
