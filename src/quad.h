ea5fce668d6d7433e95086c0fd2f7bc6c3768a47
Zalan Fasanga <zalan.fasanga@gmail.com>
2025-06-29 23:50:25 -0400

    Implemented bound checking for quad hit points.

--- src/quad.h
+++ src/quad.h
@@ -13,6 +13,7 @@ class quad : public hittable {
             auto n = cross(u, v);
             normal  = unit_vector(n);
             D = dot(normal, Q);
+            w = n / dot(n,n);
 
             set_bounding_box();
         }
@@ -38,8 +39,16 @@ class quad : public hittable {
             if (!ray_t.contains(t))
                 return false;
             
+            // Determine if the hit point lies within the planar shape using its plane coordinates.
             auto intersection = r.at(t);
+            vec3 planar_hitpt_vector = intersection - Q; // p
+            auto alpha = dot(w, cross(planar_hitpt_vector, v));
+            auto beta = dot(w, cross(u, planar_hitpt_vector));
 
+            if (!is_interior(alpha, beta, rec))
+                return false;
+            
+            // Ray hits the 2D shape; set the rest of the hit record and return true; 
             rec.t = t;
             rec.p = intersection;
             rec.mat = mat;
@@ -48,10 +57,23 @@ class quad : public hittable {
             return true;
         }
 
+        virtual bool is_interior(double a, double b, hit_record& rec) const {
+            interval unit_interval = interval(0, 1);
+            // Given the hit point in plane coordinates, return false if it is outside the
+            // primitive, otherwise set the hit record UV coordinates and return true.
+
+            if (!unit_interval.contains(a) || !unit_interval.contains(b))
+                return false;
+            
+            rec.u = a;
+            rec.v = b;
+            return true;
+        }
+
     private:
         point3 Q;
-        vec3 u;
-        vec3 v;
+        vec3 u, v;
+        vec3 w;
         shared_ptr<material> mat;
         aabb bbox;
         vec3 normal;
