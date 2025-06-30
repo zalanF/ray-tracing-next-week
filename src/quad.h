a04ca9ca5987aae408bb4eca2b1cb091f9b4db40
Zalan Fasanga <zalan.fasanga@gmail.com>
2025-06-29 23:23:34 -0400

    Implemented hit detection for quad

--- src/quad.h
+++ src/quad.h
@@ -10,6 +10,10 @@ class quad : public hittable {
         quad(const point3& Q, const vec3& u, const vec3& v, shared_ptr<material> mat) 
             : Q(Q), u(u), v(v), mat(mat) 
         {
+            auto n = cross(u, v);
+            normal  = unit_vector(n);
+            D = dot(normal, Q);
+
             set_bounding_box();
         }
 
@@ -23,7 +27,25 @@ class quad : public hittable {
         aabb bounding_box() const override { return bbox; }
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-            return false;
+            auto denom = dot(normal, r.direction());
+
+            // No hit if the ray is parallel to the plane.
+            if (std::fabs(denom) < 1e-8)
+                return false;
+
+            // Return false if the hit point parameter t is outside the ray interval.
+            auto t = (D - dot(normal, r.origin())) / denom;
+            if (!ray_t.contains(t))
+                return false;
+            
+            auto intersection = r.at(t);
+
+            rec.t = t;
+            rec.p = intersection;
+            rec.mat = mat;
+            rec.set_face_normal(r, normal);
+
+            return true;
         }
 
     private:
@@ -32,6 +54,8 @@ class quad : public hittable {
         vec3 v;
         shared_ptr<material> mat;
         aabb bbox;
+        vec3 normal;
+        double D;
 };
 
 #endif
